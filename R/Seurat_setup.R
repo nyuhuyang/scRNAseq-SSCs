18ac707f348854065f4b22ba526d7c9e3d94496a
Yang Hu <nyuhuyang@gmail.com>
2018-06-20 23:53:52 -0400

    update Seurat_setup.R

--- R/Seurat_setup.R
+++ R/Seurat_setup.R
@@ -13,7 +13,7 @@ source("./R/Seurat_functions.R")
 # 
 # ######################################################################
 #======1.1 Setup the Seurat objects =========================
-# Load the mouse.eyes dataset
+# Load the dataset
 
 # setup Seurat objects since both count matrices have already filtered
 # cells, we do no additional filtering here
@@ -26,17 +26,17 @@ conditions <- c("Adault-SSCs","Adault-SSCs","first-wave","first-wave",
 
 for(i in 1:length(samples)){
     SSCs_raw[[i]] <- Read10X(data.dir = paste0("./data/",
-                                                     samples[i],"/outs/filtered_gene_bc_matrices/hg19/"))
-    colnames(SSCs_raw[[i]]) <- paste0(conditions[i],
-                                            "_",colnames(SSCs_raw[[i]]))
+                                                     samples[i],"/outs/filtered_gene_bc_matrices/mm10/"))
+    colnames(SSCs_raw[[i]]) <- paste0(samples[i],"-",colnames(SSCs_raw[[i]]))
     SSCs_Seurat[[i]] <- CreateSeuratObject(SSCs_raw[[i]],
-                                                 min.cells = 3,
-                                                 min.genes = 200)
+                                           min.cells = 3,
+                                           min.genes = 200,
+                                           names.delim = "-")
     SSCs_Seurat[[i]]@meta.data$conditions <- conditions[i]
 }
 SSCs_Seurat <- lapply(SSCs_Seurat, FilterCells, 
                             subset.names = "nGene", 
-                            low.thresholds = 500, 
+                            low.thresholds = 200, 
                             high.thresholds = Inf)
 SSCs_Seurat <- lapply(SSCs_Seurat, NormalizeData)
 SSCs_Seurat <- lapply(SSCs_Seurat, ScaleData)
@@ -45,21 +45,23 @@ SSCs_Seurat <- lapply(SSCs_Seurat, FindVariableGenes, do.plot = FALSE)
 # we will take the union of the top 1k variable genes in each dataset for
 # alignment note that we use 1k genes in the manuscript examples, you can
 # try this here with negligible changes to the overall results
-g <- lapply(SSCs_Seurat, function(x) head(rownames(x@hvg.info), 1000))
-genes.use <- unique(c(g[[1]],g[[2]]))
-for(i in 1:length(conditions)){
-    genes.use <- intersect(genes.use, rownames(SSCs_Seurat[[i]]@scale.data))
+genes.use <- lapply(SSCs_Seurat, function(x) head(rownames(x@hvg.info), 600))
+genes.use <- unique(unlist(genes.use))
+for(i in 1:length(samples)){
+        genes.use <- intersect(genes.use, rownames(SSCs_Seurat[[i]]@scale.data))
 }
-length(genes.use)
+length(genes.use) # 1/10 of total sample size
+
 
 #======1.2 Perform a canonical correlation analysis (CCA) =========================
 # run a canonical correlation analysis to identify common sources
 # of variation between the two datasets.
 remove(SSCs_raw)
 GC()
-SSCs <- RunCCA(SSCs_Seurat[[1]],SSCs_Seurat[[2]],
-                     genes.use = genes.use,
-                     num.cc = 30)
+SSCs <- RunMultiCCA(object.list = SSCs_Seurat, 
+                    genes.use = genes.use,
+                    niter = 25, num.ccs = 30,
+                    standardize =TRUE)
 save(SSCs, file = "./data/SSCs_alignment.Rda")
 
 # CCA plot CC1 versus CC2 and look at a violin plot
@@ -86,7 +88,7 @@ SSCs <- CalcVarExpRatio(object = SSCs, reduction.type = "pca",
                               grouping.var = "conditions", dims.use = 1:20)
 SSCs <- SubsetData(SSCs, subset.name = "var.ratio.pca",accept.low = 0.5)
 
-mito.genes <- grep(pattern = "^MT-", x = rownames(x = SSCs@data), value = TRUE)
+mito.genes <- grep(pattern = "^mt-", x = rownames(x = SSCs@data), value = TRUE)
 percent.mito <- Matrix::colSums(SSCs@raw.data[mito.genes, ])/Matrix::colSums(SSCs@raw.data)
 SSCs <- AddMetaData(object = SSCs, metadata = percent.mito, col.name = "percent.mito")
 #SSCs <- ScaleData(object = SSCs, genes.use = genes.use, display.progress = FALSE, 
@@ -95,24 +97,23 @@ SSCs <- AddMetaData(object = SSCs, metadata = percent.mito, col.name = "percent.
 VlnPlot(object = SSCs, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
 
 SSCs <- FilterCells(object = SSCs, subset.names = c("nGene", "percent.mito"), 
-                          low.thresholds = c(500, -Inf), high.thresholds = c(5000, 0.10))
+                          low.thresholds = c(500, -Inf), high.thresholds = c(8000, 0.15))
 
 par(mfrow = c(1, 2))
 GenePlot(object = SSCs, gene1 = "nUMI", gene2 = "percent.mito")
 GenePlot(object = SSCs, gene1 = "nUMI", gene2 = "nGene")
 
-#Benjamin <- AddMetaData(object = Benjamin, metadata = percent.mito, col.name = "percent.mito")
 #======1.4 align seurat objects =========================
 #Now we align the CCA subspaces, which returns a new dimensional reduction called cca.aligned
 set.seed(42)
 SSCs <- AlignSubspace(object = SSCs, reduction.type = "cca", grouping.var = "conditions", 
-                            dims.align = 1:20)
+                            dims.align = 1:10)
 #Now we can run a single integrated analysis on all cells!
 
-SSCs <- FindClusters(object = SSCs, reduction.type = "cca.aligned", dims.use = 1:20, 
+SSCs <- FindClusters(object = SSCs, reduction.type = "cca.aligned", dims.use = 1:10, 
                            resolution = 0.6, force.recalc = T, save.SNN = TRUE)
 
-SSCs <- RunTSNE(object = SSCs, reduction.use = "cca.aligned", dims.use = 1:20, 
+SSCs <- RunTSNE(object = SSCs, reduction.use = "cca.aligned", dims.use = 1:10, 
                       do.fast = TRUE)
 
 p1 <- TSNEPlot(SSCs, do.return = T, pt.size = 1, group.by = "conditions")

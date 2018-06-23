e70ca554321a3188e6824083b2534ad2900873de
Yang Hu <nyuhuyang@gmail.com>
2018-06-23 18:24:53 -0400

    update Seurat_setup.R

--- R/Identify_Cell_Types_Manually.R
+++ R/Identify_Cell_Types_Manually.R
@@ -3,10 +3,10 @@ library(dplyr)
 source("./R/Seurat_functions.R")
 
 #====== 2.1 identify phenotype for each cluster  ==========================================
-lnames = load(file = "./data/MCL_alignment.Rda")
+lnames = load(file = "./data/SSCs_alignment_8.Rda")
 lnames
 
-Featureplot <- function(x,object = MCL,...){
+Featureplot <- function(x,object = SSCs,...){
     p <- FeaturePlot(object = object, 
                      reduction.use = "tsne",
                      features.plot = x, min.cutoff = NA, 
@@ -14,70 +14,70 @@ Featureplot <- function(x,object = MCL,...){
     return(p)
 }
 #------
-Adipocytes <- HumanGenes(MCL,c("SLC36A2","P2RX5","MYF5","UCP1","TRIP4","ASCC1"))
-Endothelium <- HumanGenes(MCL,c("Cdh5","Pecam1","Flt1","Plvap","Kdr","ptprb",
+Adipocytes <- HumanGenes(SSCs,c("SLC36A2","P2RX5","MYF5","UCP1","TRIP4","ASCC1"))
+Endothelium <- HumanGenes(SSCs,c("Cdh5","Pecam1","Flt1","Plvap","Kdr","ptprb",
                                   "Vwf","EMCN","Car4"))
-Epithelium <- HumanGenes(MCL,c("Epcam","KRT19","KRT5",
+Epithelium <- HumanGenes(SSCs,c("Epcam","KRT19","KRT5",
                                  "MUC1","SCGB3A2","SCGB1A1","SCGB3A1","SFTPB","FOXJ1","Rpe65",
                                  "Rlbp1","Msln","Upk3b","Lrrn4"))
-RPE <- HumanGenes(MCL,c("Rpe65","Rlbp1"))
-Fibroblast <- HumanGenes(MCL,c("FGF1","FGF9","SFRP1"))
+RPE <- HumanGenes(SSCs,c("Rpe65","Rlbp1"))
+Fibroblast <- HumanGenes(SSCs,c("FGF1","FGF9","SFRP1"))
 #--Hematopoietic----
-Hematopoietic <- HumanGenes(MCL,c("PTPRC","LAPTM5","SRGN"))
+Hematopoietic <- HumanGenes(SSCs,c("PTPRC","LAPTM5","SRGN"))
 #------Myeloid----
-megakaryocytes <-  HumanGenes(MCL,c("PPBP","GNG11"))
-erythrocyte <-  HumanGenes(MCL,c("HBA2","HBB"))
-MastCells <- HumanGenes(MCL,c("Cma1","Mcpt4","Tpsb2","Cpa3"))
-Neutrophil <- HumanGenes(MCL,c("ADAM8","MSMO1","FUT4","FCGR3A","CEACAM8"))
-CD14_Monocytes <-  HumanGenes(MCL,c("CD14","LYZ","S100A9","CCL2"))
-CD16_Monocytes <- HumanGenes(MCL,c("FCGR3A","MS4A7","VMO1"))
-Macrophages <- HumanGenes(MCL,c("LYZ","CD68","MARCO","Emr1"))
-DendriticCells <- HumanGenes(MCL,c("Itgax","GPR183","CST3","HLA-DQA1","FCER1A","TSPAN13",
+megakaryocytes <-  HumanGenes(SSCs,c("PPBP","GNG11"))
+erythrocyte <-  HumanGenes(SSCs,c("HBA2","HBB"))
+MastCells <- HumanGenes(SSCs,c("Cma1","Mcpt4","Tpsb2","Cpa3"))
+Neutrophil <- HumanGenes(SSCs,c("ADAM8","MSMO1","FUT4","FCGR3A","CEACAM8"))
+CD14_Monocytes <-  HumanGenes(SSCs,c("CD14","LYZ","S100A9","CCL2"))
+CD16_Monocytes <- HumanGenes(SSCs,c("FCGR3A","MS4A7","VMO1"))
+Macrophages <- HumanGenes(SSCs,c("LYZ","CD68","MARCO","Emr1"))
+DendriticCells <- HumanGenes(SSCs,c("Itgax","GPR183","CST3","HLA-DQA1","FCER1A","TSPAN13",
                                      "IL3RA","IGJ"))
-Myeloid <-  HumanGenes(MCL,c(megakaryocytes,erythrocyte,MastCells,
+Myeloid <-  HumanGenes(SSCs,c(megakaryocytes,erythrocyte,MastCells,
                                Monocytes,Macrophages,DendriticCells))
 #------Lymphoid----
-Lymphoid <- HumanGenes(MCL,c("Cd19","CD79A","MS4A1",
+Lymphoid <- HumanGenes(SSCs,c("Cd19","CD79A","MS4A1",
                                "GNLY","Ncr1","CCL5","KLRD1","NKG7"))
 # T cell
-T_Cell <- HumanGenes(MCL,c("CD2","CD3G","CD3D","CD8A","IL2RA","FOXP3"))
-Treg <- HumanGenes(MCL,c("FOXP3","CTLA4","PDCD1","ENTPD1","CD38",
+T_Cell <- HumanGenes(SSCs,c("CD2","CD3G","CD3D","CD8A","IL2RA","FOXP3"))
+Treg <- HumanGenes(SSCs,c("FOXP3","CTLA4","PDCD1","ENTPD1","CD38",
                          "ICOS","TNFSF9","TNFRSF9"))
-CD4_Naive_T <- HumanGenes(MCL,c("CD4","IL7R","GIMAP5","SELL","IL2RG"))
-NK <- HumanGenes(MCL,c("NKG7","CCL5"))
-Regulatory_T <-  HumanGenes(MCL,c("CD4","IL2RA","FOXP3"))
-Natural_killer_T <-  HumanGenes(MCL,c("NCAM1","FCGR3A"))
+CD4_Naive_T <- HumanGenes(SSCs,c("CD4","IL7R","GIMAP5","SELL","IL2RG"))
+NK <- HumanGenes(SSCs,c("NKG7","CCL5"))
+Regulatory_T <-  HumanGenes(SSCs,c("CD4","IL2RA","FOXP3"))
+Natural_killer_T <-  HumanGenes(SSCs,c("NCAM1","FCGR3A"))
 # B cell
-B_Cell <-HumanGenes(MCL,c("CD19","MS4A1","CD79A","CD40","CD22","FCER2",
+B_Cell <-HumanGenes(SSCs,c("CD19","MS4A1","CD79A","CD40","CD22","FCER2",
                           "HLA-DRB1","CXCR4"))
-B_StemCell <- HumanGenes(MCL,c("SPN","CD20"))
-Pre_Pro_B <- HumanGenes(MCL,c("CD34","MME","CD38"))
-Pro_B <- HumanGenes(MCL,c("MME","CD19","SPN","CD38","CD24","IL7","IL3RA"))
-Pre_B <- HumanGenes(MCL,c("MME","CD19","MS4A1","CD24","CD38","IL7","IL3RA","IL4R"))
-Immature_B <- HumanGenes(MCL,c("MME","CD19","MS4A1","CR2","CD40","CD24","CD38","IL4R"))
-Transitional_B <- HumanGenes(MCL,c("CD19","MS4A1","CD5","CR2","CD24","CD38"))
-Marginal_zone_B <- HumanGenes(MCL,c("CD1C","CD19","MS4A1","CR2","CD27"))
-Regulatory_B <- HumanGenes(MCL,c("CD1D","CD5","CD19","CR2","CD24"))
-Follicular_B <- HumanGenes(MCL,c("CD19","MS4A1","CR2","CD22","FCER2","CD24",
+B_StemCell <- HumanGenes(SSCs,c("SPN","CD20"))
+Pre_Pro_B <- HumanGenes(SSCs,c("CD34","MME","CD38"))
+Pro_B <- HumanGenes(SSCs,c("MME","CD19","SPN","CD38","CD24","IL7","IL3RA"))
+Pre_B <- HumanGenes(SSCs,c("MME","CD19","MS4A1","CD24","CD38","IL7","IL3RA","IL4R"))
+Immature_B <- HumanGenes(SSCs,c("MME","CD19","MS4A1","CR2","CD40","CD24","CD38","IL4R"))
+Transitional_B <- HumanGenes(SSCs,c("CD19","MS4A1","CD5","CR2","CD24","CD38"))
+Marginal_zone_B <- HumanGenes(SSCs,c("CD1C","CD19","MS4A1","CR2","CD27"))
+Regulatory_B <- HumanGenes(SSCs,c("CD1D","CD5","CD19","CR2","CD24"))
+Follicular_B <- HumanGenes(SSCs,c("CD19","MS4A1","CR2","CD22","FCER2","CD24",
                                    "HLA-DRB1","HLA-DQB1","HLA-DRA","HLA-DQA1"))
-Activated_B <- HumanGenes(MCL,c("CD27","CD19","MS4A1","IL2RA","TNFRSF8","CD69","CD80","CD86","FLT3"))
-Germinal_center_B <- HumanGenes(MCL,c("MME","CD19","MS4A1","FCER2","CD27","CD38","TNFRSF17"))
-Plasma_blast <- HumanGenes(MCL,c("CD19","CD38","CD27","TNFRSF17","HLA-DRB1"))
-Plasma_cell_long_lived <- HumanGenes(MCL,c("CXCR4","CD27","CD38","CD138","CD269"))
-Memory_B <- HumanGenes(MCL,c("CD19","MS4A1","CD40","CD27","CXCR4","CXCR5","ACKR3"))
+Activated_B <- HumanGenes(SSCs,c("CD27","CD19","MS4A1","IL2RA","TNFRSF8","CD69","CD80","CD86","FLT3"))
+Germinal_center_B <- HumanGenes(SSCs,c("MME","CD19","MS4A1","FCER2","CD27","CD38","TNFRSF17"))
+Plasma_blast <- HumanGenes(SSCs,c("CD19","CD38","CD27","TNFRSF17","HLA-DRB1"))
+Plasma_cell_long_lived <- HumanGenes(SSCs,c("CXCR4","CD27","CD38","CD138","CD269"))
+Memory_B <- HumanGenes(SSCs,c("CD19","MS4A1","CD40","CD27","CXCR4","CXCR5","ACKR3"))
 
 
-Melanocytes <- HumanGenes(MCL,c("Pmel","Mlana"))
-Mesenchymal <- HumanGenes(MCL,c("Pdgfrb","Vim","Has2","Dcn"))
-Myelinating_Schwann_cells <- HumanGenes(MCL,c("MBP","MPZ"))
-Pericytes <- HumanGenes(MCL,c("Pdgfrb","Cspg4","Anpep","Rgs5",
+Melanocytes <- HumanGenes(SSCs,c("Pmel","Mlana"))
+Mesenchymal <- HumanGenes(SSCs,c("Pdgfrb","Vim","Has2","Dcn"))
+Myelinating_Schwann_cells <- HumanGenes(SSCs,c("MBP","MPZ"))
+Pericytes <- HumanGenes(SSCs,c("Pdgfrb","Cspg4","Anpep","Rgs5",
                                 "Myh11","Mylk","Des","Vtn","Ifitm1"))
-Smooth_muscle_cells <- HumanGenes(MCL,c("Acta2","Myh11"))
-Stem_cell <- HumanGenes(MCL,c("POU5F1","FUT4","CD34","PROM1","ABCG2","Runx1","ATXN1",
+Smooth_muscle_cells <- HumanGenes(SSCs,c("Acta2","Myh11"))
+Stem_cell <- HumanGenes(SSCs,c("POU5F1","FUT4","CD34","PROM1","ABCG2","Runx1","ATXN1",
                                 "Nes","NCAM","NGFR"))
-Stromal_fibroblasts <- HumanGenes(MCL,c("DCN","COL6A1","TIMP3","PDGFRA"))
-Neurons <- HumanGenes(MCL,c("Ihh","Gli1", "Ptch1", "Hhip"))
-cellcycle <- HumanGenes(MCL,c("CCND1","CCND2", "CCND3", "CDK4",
+Stromal_fibroblasts <- HumanGenes(SSCs,c("DCN","COL6A1","TIMP3","PDGFRA"))
+Neurons <- HumanGenes(SSCs,c("Ihh","Gli1", "Ptch1", "Hhip"))
+cellcycle <- HumanGenes(SSCs,c("CCND1","CCND2", "CCND3", "CDK4",
                               "CDK6","PCNA","SOX11"))
 # Featureplot
 Featureplot(Adipocytes) # Adipocytes
@@ -134,17 +134,17 @@ markers.to.plot <- c(CD14_Monocytes[c(1:3)],DendriticCells[c(3,5)],
 markers.to.plot <- c("CD14","LYZ","S100A9","CST3","CD68","FCER1A","FCGR3A","MS4A7","VMO1",
                      "CD2","CD3G","CD3D","CD8A","CD19","MS4A1","CD79A","CD40","CD22",
                      "FCER2")
-markers.to.plot <- HumanGenes(MCL,markers.to.plot,unique=T)
-DotPlot(MCL, genes.plot = rev(markers.to.plot),
+markers.to.plot <- HumanGenes(SSCs,markers.to.plot,unique=T)
+DotPlot(SSCs, genes.plot = rev(markers.to.plot),
         cols.use = c("blue","red"), x.lab.rot = T, plot.legend = F,
         dot.scale = 8, do.return = T)
 #  test FindAllMarkers
-AllMarkers <- FindAllMarkers.UMI(MCL)
+AllMarkers <- FindAllMarkers.UMI(SSCs)
 AllMarkers <- AllMarkers[AllMarkers$avg_logFC > 0,]
 write.csv(AllMarkers,"./output/AllMarkers_clusters.csv")
 # Rename ident
-table(MCL@ident)
-idents <- as.data.frame(table(MCL@ident))
+table(SSCs@ident)
+idents <- as.data.frame(table(SSCs@ident))
 old.ident.ids <- idents$Var1
 new.cluster.ids <- c("T cells 0",
                      "CD14 Monocytes 1",
@@ -157,19 +157,19 @@ new.cluster.ids <- c("T cells 0",
                      "Dendritic Cells 8",
                      "Myeloid cells 9")
 
-MCL@ident <- plyr::mapvalues(x = MCL@ident,
+SSCs@ident <- plyr::mapvalues(x = SSCs@ident,
                                     from = old.ident.ids,
                                     to = new.cluster.ids)
-DotPlot(MCL, genes.plot = rev(markers.to.plot),
+DotPlot(SSCs, genes.plot = rev(markers.to.plot),
         cols.use = c("blue","red"), x.lab.rot = T, plot.legend = F,
         dot.scale = 8, do.return = T)
-# MCL <- RenameIdentBack(MCL)
+# SSCs <- RenameIdentBack(SSCs)
 
 #====== 2.2 dot Plots ==========================================
-lnames = load(file = "./data/MCL_alignment.Rda")
+lnames = load(file = "./data/SSCs_alignment.Rda")
 lnames
-table(MCL@ident)
-idents <- as.data.frame(table(MCL@ident))
+table(SSCs@ident)
+idents <- as.data.frame(table(SSCs@ident))
 old.ident.ids <- idents$Var1
 new.cluster.ids <- c("T cells",
                      "CD14 Monocytes",
@@ -182,46 +182,46 @@ new.cluster.ids <- c("T cells",
                      "Dendritic Cells",
                      "Myeloid cells")
 
-MCL@ident <- plyr::mapvalues(x = MCL@ident,
+SSCs@ident <- plyr::mapvalues(x = SSCs@ident,
                                     from = old.ident.ids,
                                     to = new.cluster.ids)
-DotPlot(MCL.normal, genes.plot = rev(markers.to.plot),
+DotPlot(SSCs.normal, genes.plot = rev(markers.to.plot),
         cols.use = c("blue","red"), x.lab.rot = T, plot.legend = T,
         dot.scale = 8, do.return = T)
-MCL@meta.data$conditions <- sub("_",".",MCL@meta.data$conditions)
-SplitDotPlotGG(object=MCL, genes.plot = rev(markers.to.plot),
+SSCs@meta.data$conditions <- sub("_",".",SSCs@meta.data$conditions)
+SplitDotPlotGG(object=SSCs, genes.plot = rev(markers.to.plot),
                cols.use = c("blue","red"), x.lab.rot = T, 
                plot.legend = T, dot.scale = 8, do.return = T, 
                grouping.var = "conditions")
 FindMarkers()
-TSNEPlot(object = MCL, no.legend = TRUE, do.label = TRUE,
+TSNEPlot(object = SSCs, no.legend = TRUE, do.label = TRUE,
          do.return = TRUE, label.size = 5)+
         ggtitle("TSNE plot of major cell types")+
         theme(text = element_text(size=20),     #larger text including legend title							
               plot.title = element_text(hjust = 0.5)) #title in middle
 
-freq_table <- prop.table(x = table(MCL@ident, MCL@meta.data[, "conditions"]),
+freq_table <- prop.table(x = table(SSCs@ident, SSCs@meta.data[, "conditions"]),
 margin = 2)
 barplot(height = freq_table)
 freq_table
-table(MCL@meta.data[, "conditions"])
+table(SSCs@meta.data[, "conditions"])
 
 #====== 2.4 Compare cell type changes across conditions  ==========================================
 # the two patients profiled have very different composition
 # Compare clusters for each dataset
-SplitTSNEPlot(object = MCL)
+SplitTSNEPlot(object = SSCs)
 #  test FindAllMarkers
-AllMarkers_cells <- FindAllMarkers.UMI(MCL)
+AllMarkers_cells <- FindAllMarkers.UMI(SSCs)
 AllMarkers_cells <- AllMarkers_cells[AllMarkers_cells$avg_logFC > 0,]
 write.csv(AllMarkers_cells,"./output/AllMarkers_cells.csv")
 
 #===========answer to 6/1's email
-MCL.subsets <- SplitCells(MCL)
-MCL.subsets[[3]]
-MCL.patient <- MCL.subsets[[1]]
-MCL.normal <- MCL.subsets[[2]]
-g1 <- Featureplot(Treg[1:2],object=MCL.patient,do.return=T)
-g2 <- Featureplot(Treg[1:2],object=MCL.normal,do.return=T)
+SSCs.subsets <- SplitCells(SSCs)
+SSCs.subsets[[3]]
+SSCs.patient <- SSCs.subsets[[1]]
+SSCs.normal <- SSCs.subsets[[2]]
+g1 <- Featureplot(Treg[1:2],object=SSCs.patient,do.return=T)
+g2 <- Featureplot(Treg[1:2],object=SSCs.normal,do.return=T)
 g <- list()
 g[[1]] <- g1[[1]];g[[2]] <- g1[[2]];g[[3]] <- g2[[1]];g[[4]] <- g2[[2]];
 do.call(plot_grid, g)
--- R/Seurat_setup.R
+++ R/Seurat_setup.R
@@ -17,9 +17,11 @@ source("./R/Seurat_functions.R")
 
 # setup Seurat objects since both count matrices have already filtered
 # cells, we do no additional filtering here
+
+# rename all "_" into "-" in sample names 
 SSCs_raw <- list()
 SSCs_Seurat <- list()
-samples <- c("Ad_depleteSp","Ad_Thy1","PND14","PND18","PND18pre",
+samples <- c("Ad-depleteSp","Ad-Thy1","PND14","PND18","PND18pre",
              "PND25","PND30","PND6" )
 conditions <- c("Adault-SSCs","Adault-SSCs","first-wave","first-wave",
                 "first-wave","first-wave","first-wave","first-wave" )
@@ -27,11 +29,11 @@ conditions <- c("Adault-SSCs","Adault-SSCs","first-wave","first-wave",
 for(i in 1:length(samples)){
     SSCs_raw[[i]] <- Read10X(data.dir = paste0("./data/",
                                                      samples[i],"/outs/filtered_gene_bc_matrices/mm10/"))
-    colnames(SSCs_raw[[i]]) <- paste0(samples[i],"-",colnames(SSCs_raw[[i]]))
+    colnames(SSCs_raw[[i]]) <- paste0(samples[i],"_",colnames(SSCs_raw[[i]]))
     SSCs_Seurat[[i]] <- CreateSeuratObject(SSCs_raw[[i]],
                                            min.cells = 3,
                                            min.genes = 200,
-                                           names.delim = "-")
+                                           names.delim = "_")
     SSCs_Seurat[[i]]@meta.data$conditions <- conditions[i]
 }
 SSCs_Seurat <- lapply(SSCs_Seurat, FilterCells, 
@@ -60,33 +62,32 @@ remove(SSCs_raw)
 GC()
 SSCs <- RunMultiCCA(object.list = SSCs_Seurat, 
                     genes.use = genes.use,
-                    niter = 25, num.ccs = 30,
+                    niter = 25, num.ccs = 50,
                     standardize =TRUE)
 save(SSCs, file = "./data/SSCs_alignment.Rda")
 
 # CCA plot CC1 versus CC2 and look at a violin plot
-p1 <- DimPlot(object = SSCs, reduction.use = "cca", group.by = "conditions", 
+p1 <- DimPlot(object = SSCs, reduction.use = "cca", group.by = "orig.ident", 
               pt.size = 0.5, do.return = TRUE)
-p2 <- VlnPlot(object = SSCs, features.plot = "CC1", group.by = "conditions", 
+p2 <- VlnPlot(object = SSCs, features.plot = "CC1", group.by = "orig.ident", 
               do.return = TRUE)
 plot_grid(p1, p2)
 
-p3 <- MetageneBicorPlot(SSCs, grouping.var = "conditions", dims.eval = 1:30, 
-                        display.progress = FALSE) # run on cluster
+
+p3 <- MetageneBicorPlot(SSCs, grouping.var = "orig.ident", dims.eval = 1:50, 
+                        display.progress = TRUE, display.progress = FALSE,
+                        smooth = TRUE) # run on cluster
 p3 + geom_smooth(method = 'loess')
 
 PrintDim(object = SSCs, reduction.type = "cca", dims.print = 1:2, genes.print = 10)
 
-DimHeatmap(object = SSCs, reduction.type = "cca", cells.use = 500, dim.use = c(1:3,11:20), 
-           do.balanced = TRUE)
-
-DimHeatmap(object = SSCs, reduction.type = "cca", cells.use = 500, dim.use = 10:18, 
+DimHeatmap(object = SSCs, reduction.type = "cca", cells.use = 500, dim.use = c(1:3,32:35), 
            do.balanced = TRUE)
 
 #======1.3 QC =========================
 SSCs <- CalcVarExpRatio(object = SSCs, reduction.type = "pca",
-                              grouping.var = "conditions", dims.use = 1:20)
-SSCs <- SubsetData(SSCs, subset.name = "var.ratio.pca",accept.low = 0.5)
+                              grouping.var = "orig.ident", dims.use = 1:35)
+SSCs <- SubsetData(SSCs, subset.name = "var.ratio.pca",accept.low = 0.5) #15633 out of 15650
 
 mito.genes <- grep(pattern = "^mt-", x = rownames(x = SSCs@data), value = TRUE)
 percent.mito <- Matrix::colSums(SSCs@raw.data[mito.genes, ])/Matrix::colSums(SSCs@raw.data)
@@ -94,41 +95,59 @@ SSCs <- AddMetaData(object = SSCs, metadata = percent.mito, col.name = "percent.
 #SSCs <- ScaleData(object = SSCs, genes.use = genes.use, display.progress = FALSE, 
 #                         vars.to.regress = "percent.mito")
 #Now we can run a single integrated analysis on all cells!
-VlnPlot(object = SSCs, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
-
+VlnPlot(object = SSCs, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 1)
 SSCs <- FilterCells(object = SSCs, subset.names = c("nGene", "percent.mito"), 
-                          low.thresholds = c(500, -Inf), high.thresholds = c(8000, 0.15))
+                          low.thresholds = c(500, -Inf), high.thresholds = c(7000, 0.15))
 
 par(mfrow = c(1, 2))
-GenePlot(object = SSCs, gene1 = "nUMI", gene2 = "percent.mito")
+GenePlot(object = SSCs, gene1 = "nUMI", gene2 = "orig.ident")
 GenePlot(object = SSCs, gene1 = "nUMI", gene2 = "nGene")
 
 #======1.4 align seurat objects =========================
 #Now we align the CCA subspaces, which returns a new dimensional reduction called cca.aligned
 set.seed(42)
-SSCs <- AlignSubspace(object = SSCs, reduction.type = "cca", grouping.var = "conditions", 
-                            dims.align = 1:10)
+SSCs <- AlignSubspace(object = SSCs, reduction.type = "cca", grouping.var = "orig.ident", 
+                            dims.align = 1:35)
 #Now we can run a single integrated analysis on all cells!
 
-SSCs <- FindClusters(object = SSCs, reduction.type = "cca.aligned", dims.use = 1:10, 
-                           resolution = 0.6, force.recalc = T, save.SNN = TRUE)
+SSCs <- FindClusters(object = SSCs, reduction.type = "cca.aligned", dims.use = 1:35, 
+                           resolution = 0.2, force.recalc = T, save.SNN = TRUE)
 
-SSCs <- RunTSNE(object = SSCs, reduction.use = "cca.aligned", dims.use = 1:10, 
+SSCs <- RunTSNE(object = SSCs, reduction.use = "cca.aligned", dims.use = 1:35, 
                       do.fast = TRUE)
 
-p1 <- TSNEPlot(SSCs, do.return = T, pt.size = 1, group.by = "conditions")
-p2 <- TSNEPlot(SSCs, do.label = F, do.return = T, pt.size = 1)
+p1 <- TSNEPlot(SSCs, do.return = T, pt.size = 1, group.by = "orig.ident")
+p2 <- TSNEPlot(SSCs, do.return = T, pt.size = 1, group.by = "ident")
 #png('./output/TSNESplot_alignment.png')
 plot_grid(p1, p2)
 #dev.off()
-
 TSNEPlot(object = SSCs,do.label = TRUE, group.by = "ident", 
          do.return = TRUE, no.legend = TRUE,
          pt.size = 1,label.size = 8 )+
-    ggtitle("TSNEplot for all cell clusters")+
-    theme(text = element_text(size=20),     #larger text including legend title							
-          plot.title = element_text(hjust = 0.5)) #title in middle
+        ggtitle("TSNEplot for all cell clusters")+
+        theme(text = element_text(size=20),     #larger text including legend title							
+              plot.title = element_text(hjust = 0.5)) #title in middle
 
-SplitTSNEPlot(object = SSCs)
-#dev.off()
-save(SSCs, file = "./data/SSCs_alignment.Rda")
+
+#======1.5 RunPCA==================================
+SSCs <- FindVariableGenes(object = SSCs, mean.function = ExpMean, dispersion.function = LogVMR, 
+                         do.plot = FALSE)
+hv.genes <- head(rownames(SSCs@hvg.info), 1000)
+SSCs <- RunPCA(object = SSCs, pc.genes = hv.genes, pcs.compute = 100, do.print = TRUE, 
+              pcs.print = 1:5, genes.print = 5)
+PCElbowPlot(object = SSCs, num.pc = 100)
+PCHeatmap(SSCs, pc.use = c(1:3, 45:50), cells.use = 500, do.balanced = TRUE)
+SSCs <- FindClusters(object = SSCs, reduction.type = "pca", dims.use = 1:50, resolution = 1, 
+                    save.SNN = TRUE, n.start = 10, nn.eps = 0.5, print.output = FALSE)
+SSCs <- RunTSNE(object = SSCs, reduction.use = "pca", dims.use = 1:50, tsne.method = "FIt-SNE", 
+               nthreads = 4, reduction.name = "FItSNE", reduction.key = "FItSNE_", 
+               fast_tsne_path = "/Users/yah2014/src/FIt-SNE/bin/fast_tsne", 
+               max_iter = 2000)
+DimPlot(object = SSCs, reduction.use = "FItSNE",do.label = TRUE,  
+        no.legend = TRUE, group.by = "orig.ident", 
+        do.return = TRUE, vector.friendly = F,
+        pt.size = 1,label.size = 8 ) + 
+        ggtitle("TSNEplot for all cell clusters")+
+        theme(text = element_text(size=20),     #larger text including legend title							
+              plot.title = element_text(hjust = 0.5)) #title in middle
+save(SSCs, file = "./data/SSCs_alignment_8.Rda")
